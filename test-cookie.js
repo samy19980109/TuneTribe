0c8034d6d082a40df0fa5a4b1a8f27e0ca753eb2
Samarth Agarwal <34971476+samy19980109@users.noreply.github.com>
2026-03-11 19:12:18 -0400

    chore: remove ad-hoc debug scripts from repo root

--- test-cookie.js
+++
@@ -1,6 +0,0 @@
-const cookieHeader = 'spotify_verifier=12345; other_cookie=abc';
-const getCookie = (name) => {
-  const match = cookieHeader.match(new RegExp('(^| )' + name + '=([^;]+)'))
-  return match ? decodeURIComponent(match[2]) : null
-}
-console.log(getCookie('spotify_verifier'));
